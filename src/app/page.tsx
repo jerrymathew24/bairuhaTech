90c5e2a1b899d4000753049d90b1769b5d2d5848
Jerry Mathew <jer2ymatheww@gmail.com>
2025-08-18 14:20:00 +0530

    page: cards added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero"
+import AiFeatures from "@/components/AiFeatures"
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
     <main>
       <Hero />
     </main>
+    <AiFeatures />
    </>
   );
 }
